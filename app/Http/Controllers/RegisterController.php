fddb21d132f8e140594fce90dcc6833bfdf8d84b
kostia7alania <osetia-alania@mail.ru>
2019-06-13 18:11:19 +0300

    store users

--- app/Http/Controllers/RegisterController.php
+++ app/Http/Controllers/RegisterController.php
@@ -4,10 +4,18 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 use App\Http\Requests\StoreUserRequest;
+use \App\User;
 
 class RegisterController extends Controller
 {
     public function register(StoreUserRequest $req) {
-        return $req;
+        $user = new User;
+
+        $user->username = $req->username;
+        $user->email = $req->email;
+        $user->password = bcrypt($req->password);
+
+        $user->save();
+        return $user;
     }
 }
